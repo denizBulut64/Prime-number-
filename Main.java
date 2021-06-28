a0e734784ae19d5be5d7b62614086a967d044c62
denizBulut64 <86578400+denizBulut64@users.noreply.github.com>
2021-06-28 03:34:48 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    //asal sayi bulma işlemi
+    public static void main(String[] args) {
+
+        int number = 0;
+        int remainder = number % 2;
+        boolean isPrime = true;
+
+        if (number == 1) {
+            System.out.println("Sayi asal degildir");
+            return;
+        }
+        if (number < 1) {
+
+            System.out.println("Geçerli bir sayi yazınız");
+            return;
+        }
+
+        for (int i = 2; i < number; i++) {
+
+
+            if (number % i == 0) {
+                isPrime = false;
+            }
+        }
+        if (isPrime = false) {
+            System.out.println("Sayi asaldir.");
+        } else {
+            System.out.println("Sayi asal sayi değildir.");
+        }
+
+    }
+}
+
